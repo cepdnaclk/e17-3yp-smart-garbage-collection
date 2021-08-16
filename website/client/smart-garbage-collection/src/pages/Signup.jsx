82f16216dd61e17679d3baab2024a71b30091649
Tillekeratne L.J.I <e17352@eng.pdn.ac.lk>
2021-08-17 00:46:15 +0530

    Complete basic sign up UI

--- website/client/smart-garbage-collection/src/pages/Signup.jsx
+++ website/client/smart-garbage-collection/src/pages/Signup.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+import Form from '../components/Form'
 
 function Signup() {
-    return <h1>Signup</h1>;
+
+    return (<Container component="main" maxWidth="xs" >
+        <CssBaseline />
+        <Form title="Sign Up" msg="Already have an account? Sign in" link="/Signin" />
+    </Container>
+    );
 }
 
 export default Signup;
