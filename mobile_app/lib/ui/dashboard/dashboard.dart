76c53d62abee3d5c7a25a09b6dee27833090f12e
VidurangiKalpana <73962748+VidurangiKalpana@users.noreply.github.com>
2021-09-03 13:35:27 +0530

    Update dashboard.dart

--- mobile_app/lib/ui/dashboard/dashboard.dart
+++ mobile_app/lib/ui/dashboard/dashboard.dart
@@ -1,207 +1,82 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:loginkit/components/rounded_btn/rounded_btn.dart';
-import 'package:loginkit/ui/create_account/create_account.dart';
-import 'package:loginkit/ui/success/success.dart';
-import 'package:modal_progress_hud/modal_progress_hud.dart';
+import 'package:confetti/confetti.dart';
+import 'dart:math';
 
-class Login extends StatefulWidget {
+class SuccessScreen extends StatefulWidget {
   @override
-  _LoginState createState() => _LoginState();
+  _SuccessScreenState createState() => _SuccessScreenState();
 }
 
-class _LoginState extends State<Login> {
-  bool showSpinner = false;
-  final _auth = FirebaseAuth.instance;
-  String email;
-  String password;
+class _SuccessScreenState extends State<SuccessScreen> {
+  ConfettiController _controllerBottomCenter;
+
+  @override
+  void initState() {
+    ConfettiController(duration: const Duration(seconds: 5));
+    _controllerBottomCenter =
+        ConfettiController(duration: const Duration(seconds: 10));
+    WidgetsBinding.instance
+        .addPostFrameCallback((_) => _controllerBottomCenter.play());
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controllerBottomCenter.dispose();
+    super.dispose();
+  }
 
 
   @override
   Widget build(BuildContext context) {
-    return ModalProgressHUD(
-      inAsyncCall: showSpinner,
-      child: SafeArea(
-        child: Scaffold(
-          resizeToAvoidBottomPadding: true,
-          backgroundColor: Color(0xff251F34),
-          body: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Center(
-                  child: SizedBox(
-                    width: 175,
-                    height: 175,
-                    child: SvgPicture.asset('images/login.svg')
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(20, 15, 20, 8),
-                  child: Text('Login',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w600,
-                    fontSize: 20
-                  ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                  child: Text('Please sign in to continue.',
-                      style: TextStyle(
-                      color: Colors.grey[600],
-                      fontWeight: FontWeight.w400,
-                      fontSize: 13
-                  ),
-        ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                  child: Container(
-                    margin: EdgeInsets.symmetric(vertical: 10),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          'E-mail',
-                          style: TextStyle(fontWeight: FontWeight.w300, fontSize: 13, color: Colors.white),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        TextField(
-                          style: (TextStyle(
-                           color: Colors.white,
-                           fontWeight: FontWeight.w400
-                          )),
-                          keyboardType: TextInputType.emailAddress,
-                            cursorColor: Colors.white,
-                            obscureText: false,
-                            decoration: InputDecoration(
-                                border: InputBorder.none,
-                                fillColor: Color(0xfff3B324E),
-                                filled: true,
-                            prefixIcon: Image.asset('images/icon_email.png'),
-                              focusedBorder: OutlineInputBorder(
-                                borderSide: BorderSide(color: Color(0xff14DAE2), width: 2.0),
-                                borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                              ),
-                        ),
-                          onChanged: (value) {
-                            email = value;
-                          },
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        'Password',
-                        style: TextStyle(fontWeight: FontWeight.w300, fontSize: 13, color: Colors.white),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      TextField(
-                        style: (TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.w400
-                        )),
-                        obscureText: true,
-                        cursorColor: Colors.white,
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          fillColor: Color(0xfff3B324E),
-                          filled: true,
-                          prefixIcon: Image.asset('images/icon_lock.png'),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Color(0xff14DAE2), width: 2.0),
-                            borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                          ),
-                        ),
-                        onChanged: (value) {
-                          password = value;
-                        },
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Center(
-                    child: RoundedButton(
-                      btnText: 'LOGIN',
-                      color: Color(0xff14DAE2),
-                      onPressed: () async {
-                        // Add login code
-                        setState(() {
-                          showSpinner = true;
-                        });
-                        try {
-                          final user = await _auth.signInWithEmailAndPassword(
-                              email: email, password: password);
-                          if (user != null) {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => SuccessScreen()));
-                          }
-                          setState(() {
-                            showSpinner = false;
-                          });
-                        } catch (e) {
-                          print(e);
-                        }
-                      },
-                    ),
-                  ),
-                ),
-                Center(
-                  child: Text('Forgot Password?',
-                  style: TextStyle(
-                    color: Color(0xff14DAE2)
-                  ),
-                  ),
-                ),
-                SizedBox(
-                  height: 25,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('Dont have an account?',
-                      style: TextStyle(
-                          color: Colors.grey[600],
-                          fontWeight: FontWeight.w400
-                      ),),
-                    FlatButton(
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => CreateAccount()));
-                      },
-                      child: Text('Sign up',
-                        style: TextStyle(
-                            color: Color(0xff14DAE2),)
-                      ),
-                    )
-                  ],
-                )
-              ],
+    return Scaffold(
+      backgroundColor: Color(0xff251F34),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Align(
+            alignment: Alignment.topCenter,
+            child: ConfettiWidget(
+              confettiController: _controllerBottomCenter,
+              blastDirection: pi / 2,
+              maxBlastForce: 3, // set a lower max blast force
+              minBlastForce: 2,
+              emissionFrequency: 0.3,
+              minimumSize: const Size(10,
+                  10), // set the minimum potential size for the confetti (width, height)
+              maximumSize: const Size(20,
+                  20), // set the maximum potential size for the confetti (width, height)
+              numberOfParticles: 1,
+              gravity: 1,
             ),
           ),
-        ),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Text('Congratulations,',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 27
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text('You have logged in.',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 23
+              ),
+            ),
+          ),
+          SizedBox(
+              height: 200,
+              width: 200,
+              child: SvgPicture.asset('images/success.svg')),
+        ],
       ),
     );
   }
