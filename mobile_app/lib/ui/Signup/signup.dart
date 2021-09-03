eb65dc47c1d702ee6f336f780502a892ee8cb71b
VidurangiKalpana <73962748+VidurangiKalpana@users.noreply.github.com>
2021-09-03 13:34:47 +0530

    Update signup.dart

--- mobile_app/lib/ui/Signup/signup.dart
+++ mobile_app/lib/ui/Signup/signup.dart
@@ -1,208 +1,199 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:loginkit/components/rounded_btn/rounded_btn.dart';
-import 'package:loginkit/ui/create_account/create_account.dart';
+import 'package:loginkit/ui/login/login.dart';
 import 'package:loginkit/ui/success/success.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
 
-class Login extends StatefulWidget {
+class CreateAccount extends StatefulWidget {
   @override
-  _LoginState createState() => _LoginState();
+  _CreateAccountState createState() => _CreateAccountState();
 }
 
-class _LoginState extends State<Login> {
+class _CreateAccountState extends State<CreateAccount> {
   bool showSpinner = false;
   final _auth = FirebaseAuth.instance;
   String email;
   String password;
 
-
   @override
   Widget build(BuildContext context) {
     return ModalProgressHUD(
       inAsyncCall: showSpinner,
-      child: SafeArea(
-        child: Scaffold(
-          resizeToAvoidBottomPadding: true,
+      child: Scaffold(
+        resizeToAvoidBottomPadding: true,
+        appBar: AppBar(
+          elevation: 0,
+            leading: _goBackButton(context),
           backgroundColor: Color(0xff251F34),
-          body: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Center(
-                  child: SizedBox(
-                    width: 175,
-                    height: 175,
-                    child: SvgPicture.asset('images/login.svg')
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(20, 15, 20, 8),
-                  child: Text('Login',
+        ),
+        backgroundColor: Color(0xff251F34),
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 20, 20, 8),
+                child: Text('Create Account',
                   style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w600,
-                    fontSize: 20
-                  ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                  child: Text('Please sign in to continue.',
-                      style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 25
+                  ),),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: Text('Please fill the input below.',
+                  style: TextStyle(
                       color: Colors.grey[600],
                       fontWeight: FontWeight.w400,
-                      fontSize: 13
-                  ),
-        ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                  child: Container(
-                    margin: EdgeInsets.symmetric(vertical: 10),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          'E-mail',
-                          style: TextStyle(fontWeight: FontWeight.w300, fontSize: 13, color: Colors.white),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        TextField(
-                          style: (TextStyle(
-                           color: Colors.white,
-                           fontWeight: FontWeight.w400
-                          )),
-                          keyboardType: TextInputType.emailAddress,
-                            cursorColor: Colors.white,
-                            obscureText: false,
-                            decoration: InputDecoration(
-                                border: InputBorder.none,
-                                fillColor: Color(0xfff3B324E),
-                                filled: true,
-                            prefixIcon: Image.asset('images/icon_email.png'),
-                              focusedBorder: OutlineInputBorder(
-                                borderSide: BorderSide(color: Color(0xff14DAE2), width: 2.0),
-                                borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                              ),
-                        ),
-                          onChanged: (value) {
-                            email = value;
-                          },
-                        ),
-                      ],
+                      fontSize: 14
+                  ),),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'E-mail',
+                      style: TextStyle(fontWeight: FontWeight.w300, fontSize: 13, color: Colors.white),
                     ),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        'Password',
-                        style: TextStyle(fontWeight: FontWeight.w300, fontSize: 13, color: Colors.white),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      TextField(
-                        style: (TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.w400
-                        )),
-                        obscureText: true,
-                        cursorColor: Colors.white,
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          fillColor: Color(0xfff3B324E),
-                          filled: true,
-                          prefixIcon: Image.asset('images/icon_lock.png'),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Color(0xff14DAE2), width: 2.0),
-                            borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                          ),
-                        ),
-                        onChanged: (value) {
-                          password = value;
-                        },
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Center(
-                    child: RoundedButton(
-                      btnText: 'LOGIN',
-                      color: Color(0xff14DAE2),
-                      onPressed: () async {
-                        // Add login code
-                        setState(() {
-                          showSpinner = true;
-                        });
-                        try {
-                          final user = await _auth.signInWithEmailAndPassword(
-                              email: email, password: password);
-                          if (user != null) {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => SuccessScreen()));
-                          }
-                          setState(() {
-                            showSpinner = false;
-                          });
-                        } catch (e) {
-                          print(e);
-                        }
-                      },
+                    SizedBox(
+                      height: 10,
                     ),
-                  ),
-                ),
-                Center(
-                  child: Text('Forgot Password?',
-                  style: TextStyle(
-                    color: Color(0xff14DAE2)
-                  ),
-                  ),
-                ),
-                SizedBox(
-                  height: 25,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('Dont have an account?',
-                      style: TextStyle(
-                          color: Colors.grey[600],
+                    TextField(
+                      style: (TextStyle(
+                          color: Colors.white,
                           fontWeight: FontWeight.w400
-                      ),),
-                    FlatButton(
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => CreateAccount()));
-                      },
-                      child: Text('Sign up',
-                        style: TextStyle(
-                            color: Color(0xff14DAE2),)
+                      )),
+                      keyboardType: TextInputType.emailAddress,
+                      obscureText: false,
+                      cursorColor: Colors.white,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        fillColor: Color(0xfff3B324E),
+                        filled: true,
+                        prefixIcon: Image.asset('images/icon_email.png'),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xff14DAE2), width: 2.0),
+                          borderRadius: BorderRadius.all(Radius.circular(20.0)),
                       ),
-                    )
+                    ),
+                      onChanged: (value) {
+                        email = value;
+                      },
+                    ),
                   ],
-                )
-              ],
-            ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'Password',
+                      style: TextStyle(fontWeight: FontWeight.w300, fontSize: 13, color: Colors.white),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextField(
+                      style: (TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w400
+                      )),
+                      obscureText: true,
+                      cursorColor: Colors.white,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        fillColor: Color(0xfff3B324E),
+                        filled: true,
+                        prefixIcon: Image.asset('images/icon_lock.png'),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xff14DAE2), width: 2.0),
+                          borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                        ),
+                      ),
+                      onChanged: (value) {
+                        password = value;
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                  child: RoundedButton(
+                    btnText: 'SIGN UP',
+                    color: Color(0xff14DAE2),
+                    onPressed: () async {
+                      setState(() {
+                        showSpinner = true;
+                      });
+                      try {
+                        final newUser =
+                        await _auth.createUserWithEmailAndPassword(
+                            email: email, password: password);
+                        if (newUser != null) {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => SuccessScreen()));
+                        }
+
+                        setState(() {
+                          showSpinner = false;
+                        });
+                      } catch (e) {
+                        print(e);
+                      }
+                      // Add login code
+                    },
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 100,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Already have an account?',
+                    style: TextStyle(
+                        color: Colors.grey[600],
+                        fontWeight: FontWeight.w400
+                    ),),
+                  FlatButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => Login()));
+                    },
+                    child: Text('Sign in',
+                        style: TextStyle(
+                          color: Color(0xff14DAE2),)
+                    ),
+                  )
+                ],
+              )
+            ],
           ),
         ),
       ),
     );
   }
+}
+
+Widget _goBackButton(BuildContext context) {
+  return IconButton(
+      icon: Icon(Icons.arrow_back, color: Colors.grey[350]),
+      onPressed: () {
+        Navigator.of(context).pop(true);
+      });
 }
