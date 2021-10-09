b4c18500ea85e9e1adb099f4f1434aa5780a7f21
Tillekeratne L.J.I <e17352@eng.pdn.ac.lk>
2021-10-09 15:11:07 +0530

    yup signup worked

--- Web_Frontend/src/pages/Signup.jsx
+++ Web_Frontend/src/pages/Signup.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
-//import { useHistory } from 'react-router-dom';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
@@ -12,6 +11,7 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Axios from 'axios';
 import { useHistory } from 'react-router-dom';
+import { signupSchema } from '../Validations/SignupValidation';
 
 const useStyles = makeStyles((theme) => ({
     paper: {
@@ -39,30 +39,42 @@ const useStyles = makeStyles((theme) => ({
 function Signup() {
 
     let history = useHistory();
-    //let displayMsg;
 
     const classes = useStyles();
 
-    const [fnameReg, setFameReg] = useState('');
-    const [lnameReg, setLameReg] = useState('');
-    const [usernameReg, setUsernameReg] = useState('');
-    const [passwordReg, setPasswordReg] = useState('');
-    //const [signupStatus, setSignupStatus] = useState('');
+    // const [fnameReg, setFameReg] = useState('');
+    // const [lnameReg, setLameReg] = useState('');
+    // const [usernameReg, setUsernameReg] = useState('');
+    // const [passwordReg, setPasswordReg] = useState('');
 
-    const register = () => {
-        Axios.post("http://localhost:3001/Signup", {
-            adminfname: fnameReg,
-            adminlname: lnameReg,
-            adminusername: usernameReg,
-            adminpassword: passwordReg,
-        }).then((response) => {
-            if (response.data.error) alert(response.data.error);
-            else {
-                history.push("/Signin");
-            }
-        });
+
+    // const register = () => {
+    //     Axios.post("http://localhost:3001/Signup", {
+    //         adminfname: fnameReg,
+    //         adminlname: lnameReg,
+    //         adminusername: usernameReg,
+    //         adminpassword: passwordReg,
+    //     }).then((response) => {
+    //         if (response.data.error) alert(response.data.error);
+    //         else {
+    //             history.push("/Signin");
+    //         }
+    //     });
+    // }
+
+
+    const onSubmit = async (event) => {
+        event.preventDefault()
+        let formData = {
+            firstName: event.target[0].value,
+            lastName: event.target[2].value,
+            username: event.target[4].value,
+            password: event.target[6].value
+        }
+        console.log(formData);
+        const isValid = await signupSchema.isValid(formData);
+        console.log(isValid);
     }
-
 
     return (<Container component="main" maxWidth="xs" >
         <CssBaseline />
@@ -73,43 +85,52 @@ function Signup() {
             <Typography component="h1" variant="h5">
                 Sign Up
             </Typography>
-            <Grid container spacing={2} className={classes.form}><Grid item xs={12} sm={6}>
-                <TextField
-                    autoComplete="fname"
-                    name="firstName"
-                    variant="outlined"
-                    required
-                    fullWidth
-                    id="firstName"
-                    label="First Name"
-                    autoFocus
 
-                    // take user input
-                    onChange={(e) => {
-                        setFameReg(e.target.value);
-                    }}
+            <form className={classes.form} onSubmit={onSubmit}>
 
-                />
-            </Grid>
-                <Grid item xs={12} sm={6}>
-                    <TextField
-                        variant="outlined"
-                        required
-                        fullWidth
-                        id="lastName"
-                        label="Last Name"
-                        name="lastName"
-                        autoComplete="lname"
+                <Grid container spacing={2} className={classes.form}>
+                    <Grid item xs={12} sm={6}>
+
+                        <TextField
+                            autoComplete="fname"
+                            name="firstName"
+                            variant="outlined"
+                            required
+                            fullWidth
+                            id="firstName"
+                            label="First Name"
+                            autoFocus
 
                         // take user input
-                        onChange={(e) => {
-                            setLameReg(e.target.value);
-                        }}
+                        // onChange={(e) => {
+                        //     setFameReg(e.target.value);
+                        // }}
 
-                    />
-                </Grid></Grid>
+                        />
 
-            <form className={classes.form} noValidate>
+
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+
+                        <TextField
+                            variant="outlined"
+                            fullWidth
+                            id="lastName"
+                            label="Last Name"
+                            name="lastName"
+                            autoComplete="lname"
+
+                        // take user input
+                        // onChange={(e) => {
+                        //     setLameReg(e.target.value);
+                        // }}
+
+                        />
+
+                    </Grid></Grid>
+
+
+
                 <TextField
                     variant="outlined"
                     margin="normal"
@@ -121,12 +142,14 @@ function Signup() {
                     autoComplete="username"
                     autoFocus
 
-                    onChange={(e) => {
-                        setUsernameReg(e.target.value);
-                    }}
+                // onChange={(e) => {
+                //     setUsernameReg(e.target.value);
+                // }}
 
 
                 />
+
+
                 <TextField
                     variant="outlined"
                     margin="normal"
@@ -138,24 +161,21 @@ function Signup() {
                     id="password"
                     autoComplete="current-password"
 
-                    onChange={(e) => {
-                        setPasswordReg(e.target.value);
-                    }}
+                // onChange={(e) => {
+                //     setPasswordReg(e.target.value);
+                // }}
                 />
 
 
-                {/* <FormControlLabel ### REMEMBER ME
-                        control={<Checkbox value="remember" color="primary" />}
-                        label="Remember me"
-                    /> */}
+
                 <Button
-                    // type="submit"
+                    type="submit"
                     fullWidth
                     variant="contained"
                     color="primary"
                     className={classes.submit}
-                    // href="/Dashboard"
-                    onClick={register}
+                // onClick={register}
+                // onClick={onSubmit}
 
                 >
                     Sign Up
@@ -167,12 +187,7 @@ function Signup() {
                             Already have an account? Sign in
                         </Link>
                     </Grid>
-                    <Grid item>
 
-                        {/* DID NOT WORK */}
-                        {/* {displayMsg} */}
-
-                    </Grid>
                 </Grid>
             </form>
         </div >
