a6874765f2613a127e6dddb2d1b7307f8b2fa1e7
Tillekeratne L.J.I <e17352@eng.pdn.ac.lk>
2021-10-12 15:01:22 +0530

    map started

--- Web_Frontend/src/components/TempMap.jsx
+++ Web_Frontend/src/components/TempMap.jsx
@@ -1,20 +1,24 @@
-import React from 'react';
-import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'react-google-maps';
+import * as React from 'react';
+import { useState } from 'react';
+import ReactMapGL from 'react-map-gl';
 
-function TempMap() {
-    return <Map google={this.props.google} zoom={14}>
+export default function TempMap() {
+    const [viewport, setViewport] = useState({
+        width: window.innerWidth,
+        height: window.innerHeight,
+        latitude: 6.8018,
+        longitude: 79.9227,
+        zoom: 15
+    });
 
-        <Marker onClick={this.onMarkerClick}
-            name={'Current location'} />
-
-        <InfoWindow onClose={this.onInfoWindowClose}>
-            <div>
-                <h1>{this.state.selectedPlace.name}</h1>
-            </div>
-        </InfoWindow>
-    </Map>;
+    return (
+        <ReactMapGL
+            // .env. thing didn't work
+            mapboxApiAccessToken={"pk.eyJ1IjoiaXNhcmExMjMiLCJhIjoiY2t1bXhyaHlmM3Z0dTJ1azZxdTF5anJsbiJ9.y9bx1-DVAKDocnb8ZJNSTg"}
+            mapStyle="mapbox://styles/isara123/ckumzi9ch0dzi17livce3kxp5" //outdoors
+            // mapStyle="mapbox://styles/isara123/ckumzniex5d8317mnjspf1bdr" //street
+            {...viewport}
+            onViewportChange={nextViewport => setViewport(nextViewport)}
+        />
+    );
 }
-
-export default GoogleApiWrapper({
-    apiKey: ("AIzaSyAFJY6haLkUkP0YIFsbpNZvH9fFO7qDHcw")
-})(TempMap)
