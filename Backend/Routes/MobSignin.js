4d3132a1c482bc3e1abb1a04d3e47c4b92626795
Hashini <e17398@eng.pdn.ac.lk>
2021-10-15 09:40:40 +0530

    JWT

--- Backend/Routes/MobSignin.js
+++ Backend/Routes/MobSignin.js
@@ -3,43 +3,55 @@ const db = require("../connection");
 const Router = express.Router();
 const bcrypt = require('bcrypt');
 //const { response } = require('express');
+const { createTokens, validateToken } = require('../JWT');
 
 const saultRounds = 10; // for pw hashing
 
-Router.post("/", async function(req, res) {
+Router.post("/authenticate", async function(req, res) {
 
 
     const username = req.body.collectorusername;
     const password = req.body.collectorpassword;
 
-    // if first name is not entered
-    if (username == '' || password == '') res.send({ message: 'Please enter both username & password' })
+    // if username and password is not entered
+    //if (username == '' && password == '') res.send({ message: 'Please enter both username & password' })
 
-    else {
+    //else {
 
-        db.query("SELECT * FROM collector WHERE username = ?",
-            username,
-            (err, result) => {
-                if (err) {
-                    res.send({ err: err });
+    db.query("SELECT * FROM collector WHERE username = ?",
+        username,
+        (err, result) => {
+            if (err) {
+                res.send({ err: err });
 
-                }
-                if (result.length > 0) {
-                    bcrypt.compare(password, result[0].password, (error, response) => {
-                        if (response) {
+            }
+            if (result.length > 0) {
+                bcrypt.compare(password, result[0].password).then((match) => {
+                    if (match) {
+                        const accessToken = createTokens(result[0]);
+                        res.cookie("access-token", accessToken, {
+                            maxAge: 60 * 60 * 1 * 1000,
+                            httpOnly: true // to save from attackers taking the token
 
-                            res.send({ message: 'succesful' });
-                        } else {
-                            res.send({ message: 'Incorrect password' });
-                        }
-                    })
-                } else {
-                    res.send({ message: "User doesn't exist" });
+                        });
 
-                }
-            });
+                        // cookie expire 1 hours 
+                        res.send({ message: 'successful' });
 
-    }
+                        // send the acess token to the front end
+                        //res.json(accessToken);
+                    } else {
+                        res.send({ error: "Incorrect password" });
+
+                    }
+                })
+            } else {
+                res.send({ message: "User doesn't exist" });
+
+            }
+        });
+
+    //}
 
 
 });
---
+++ Backend/Routes/collectorProfile.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const db = require("../connection");
+const Router = express.Router();
+const { createTokens, validateToken } = require('../JWT');
+
+Router.get("/Profile", validateToken, (req, res) => {
+    res.send({ message: 'successful' });
+});
+
+module.exports = Router;
